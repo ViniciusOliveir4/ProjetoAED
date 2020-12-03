458b9921c3ad195e4bfee4836b21c7eef8570549
Lucas Matos <lucaspmatos@outlook.com>
2020-12-03 00:15:34 -0300

    Início de Implementação de Funções de Veículo

--- ProjetoLocação.c
+++ ProjetoLocação.c
@@ -1,217 +1,154 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
-int codCliente=0;
+int codVeiculo = 0;
 
-typedef struct{
-    char fabricante[50];
-    char modelo[50];
-    char tipo[50];
-    char cor [20];
-    char tipoCombustivel [20];
-    char despesaLocacao[50];
-    int ano;
-    int qtd;   //quantidade de veiculos desse modelo
-    int qtdLocado;
-    int tempoLocado;
-    float consumoKm;
-    float valorLocacao;
-    float kilometragem;
-}VEICULO;
+// Entidades
+typedef struct {
+  int dia;
+  int mes;
+  int ano;
+} DIA;
 
-typedef struct{
-    int codigo;
-    char nome[80];
-    char data_nasc[40];
-    char cpf[50];
-    char endereco [80];
-    char telefone [30];
-    char email[50];
-    float descontoApp;
-    int qtdLocacoes;
-    float valorTotal;
-}CLIENTE;
+typedef struct {
+  int codigo;
+  char fabricante[50];
+  char modelo[50];
+  char tipo[50];
+  char cor[20];
+  char tipoCombustivel[20];
+  int ano;
+  float valorDia;
+  float despesaDia;
+  char status;
+} VEICULO;
 
-typedef struct{
-    VEICULO veiculo;
-    CLIENTE cliente;
-    int tempoLocacao;
-    float kilometragem;
-    float despesa;
-    float desconto;
-    float valor;
-}LOCACAO;
+typedef struct {
+  char nome[40];
+  int idade;
+  char cpf[50];
+  char endereco[80];
+  char telefone[30];
+  char email[50];
+  int qtdLocacoes;
+  VEICULO veiculoPreferido;
+  float tempoMedio;
+  float kilometragemMedia;
+  float valorLocacao;
+  int kilometragem;
+} CLIENTE;
 
-typedef struct Elem{
-    CLIENTE cli;
-    struct Elem *prox;
-}Caixa;
+typedef struct {
+  CLIENTE cliente;
+  VEICULO veiculo;
+  DIA diaInicio;
+  DIA diaDevolucao;
+  DIA diaTotal;
+  float valorDia;
+  float despesaDia;
+  float despesaTotal;
+} LOCACAO;
 
-typedef Caixa * ponteiro;
+// Listas
+typedef struct elementoVeiculo {
+  VEICULO veiculo;
+  struct elementoVeiculo *prox;
+} Veiculo;
 
-typedef struct{
-    ponteiro prim, ult;
-    int qtd;
-}Lista;
+typedef Veiculo *v;
 
-void inicializarListaCliente(Lista *C){
-    C->prim =(ponteiro)malloc(sizeof(Caixa));
-    C->ult = C->prim;
-    C->prim->prox = NULL;
-    C->qtd=0;
+typedef struct {
+  v prim, ult;
+  int qtd;
+} ListaVeiculo;
+
+void criarListaVeiculo(ListaVeiculo *lv) {
+  lv->prim = (v)malloc(sizeof(Veiculo));
+  lv->ult = lv->prim;
+  lv->prim->prox = NULL;
+  lv->qtd = 0;
 }
 
-void inserirCliente(Lista *C, CLIENTE X){
-    ponteiro p, q;
+void inserirVeiculo(ListaVeiculo *lv, VEICULO veic) {
+  v p, q;
 
-    q = (ponteiro)malloc(sizeof(Caixa));
-    q->cli = X;
+  q = (v)malloc(sizeof(Veiculo));
+  q->veiculo = veic;
 
-    p=C->prim;
-    while((p!=C->ult)&&(X.codigo>p->prox->cli.codigo))
-        p=p->prox;
-     q->prox=p->prox;
-     p->prox=q;
-     C->qtd++;
-
-     if(p==C->ult)
-         C->ult=q;
+  p = lv->prim;
+  while ((p != lv->ult) && (veic.codigo > p->prox->veiculo.codigo))
+    p = p->prox;
+    q->prox = p->prox;
+    p->prox = q;
+    lv->qtd++;
+  if (p == lv->ult)
+    lv->ult = q;
 }
 
-void exibeListaCliente(Lista C){
-    ponteiro p;
-
-    printf("\n ********");
-    printf("\n * Cab  *");
-    printf("\n * List *");
-    printf("\n ***<>***");
-    printf("\n     V   ");
-    p=C.prim->prox;
-
-    while(p!=NULL){
-        printf("\n ********");
-        printf("\n * %3d  *",p->cli.codigo);
-        printf("\n *      *");
-        printf("\n ***<>***");
-        printf("\n     V   ");
-        p=p->prox;
-    }
+void cadastrarCliente(PESSOA *P) {
+  printf("=====> CADASTRAR CLIENTE <=====\n\n");
+  printf("\nENTRE COM O NOME DO CLIENTE: ");
+  fflush(stdin);
+  fgets(P->nome, 40, stdin);
+  printf("\nENTRE COM A IDADE: ");
+  scanf("%d", &P->idade);
+  printf("\nENTRE COM O CPF: ");
+  fflush(stdin);
+  fgets(P->cpf, 50, stdin);
+  printf("\nENTRE COM O ENDERECO DO CLIENTE: ");
+  fflush(stdin);
+  fgets(P->endereco, 80, stdin);
+  printf("\nENTRE COM O TELEFONE DO CLIENTE: ");
+  fflush(stdin);
+  fgets(P->telefone, 30, stdin);
+  printf("\nENTRE COM O E-MAIL DO CLIENTE: ");
+  fflush(stdin);
+  fgets(P->email, 50, stdin);
+  printf("\n\n\n\nCLIENTE CADASTRADO COM SUCESSO\n");
+  fflush(stdin);
 }
 
-void cadastrarCliente(CLIENTE *C){
-    printf("=====> CADASTRAR CLIENTE <=====\n\n");
-    printf("\nENTRE COM O NOME DO CLIENTE: ");
-    fflush(stdin);
-    fgets(C->nome, 80, stdin);
-    printf("\nENTRE COM A DATA DE NASCIMENTO: ");
-    fflush(stdin);
-    fgets(C->data_nasc, 40, stdin);
-    printf("\nENTRE COM O CPF: ");
-    fflush(stdin);
-    fgets(C->cpf, 50, stdin);
-    printf("\nENTRE COM O ENDERECO DO CLIENTE: ");
-    fflush(stdin);
-    fgets(C->endereco, 80, stdin);
-    printf("\nENTRE COM O TELEFONE DO CLIENTE: ");
-    fflush(stdin);
-    fgets(C->telefone, 30, stdin);
-    printf("\nENTRE COM O E-MAIL DO CLIENTE: ");
-    fflush(stdin);
-    fgets(C->email, 50, stdin);
-    printf("\n\n\n\nCLIENTE CADASTRADO COM SUCESSO\n");
-    fflush(stdin);
-    C->codigo = ++codCliente;
+void cadastrarVeiculo(VEICULO *V) {
+  printf("=====> CADASTRAR VEÍCULO <=====\n\n");
+  printf("\nENTRE COM O FABRICANTE: ");
+  fflush(stdin);
+  fgets(V->fabricante, 50, stdin);
+  printf("\nENTRE COM O MODELO: ");
+  fflush(stdin);
+  fgets(V->modelo, 50, stdin);
+  printf("\nENTRE COM O TIPO: ");
+  fflush(stdin);
+  fgets(V->tipo, 50, stdin);
+  printf("\nENTRE COM O COR: ");
+  fflush(stdin);
+  fgets(V->cor, 20, stdin);
+  printf("\nENTRE COM O TIPO DE COMBUSTÍVEL: ");
+  fflush(stdin);
+  fgets(V->tipoCombustivel, 20, stdin);
+  printf("\nENTRE COM O ANO: ");
+  scanf("%d", &V->ano);
+  printf("\nENTRE COM O VALOR DO ALUGUEL POR DIA DO VEÍCULO: ");
+  scanf("%f", &V->valorDia);
+  printf("\nENTRE COM A DESPESA POR DIA DO VEÍCULO: ");
+  scanf("%d", &V->despesaDia);
+  printf("\nENTRE COM O STATUS DO VEÍCULO (D = Disponível | R = Reservado | M = Em Manutenção): ");
+  fflush(stdin);
+  fgets(V->status, 1, stdin);
+  printf("\n\n\n\nVEICULO CADASTRADO COM SUCESSO!\n");
+  fflush(stdin);
+  V->codigo = ++codVeiculo;
 }
 
-void cadastrarVeiculo(VEICULO *V){
-    printf("=====> CADASTRAR VEICULO<=====\n\n");
-    printf("\nENTRE COM O FABRICANTE: ");
-    fflush(stdin);
-    fgets(V->fabricante, 50, stdin);
-    printf("\nENTRE COM O MODELO: ");
-    fflush(stdin);
-    fgets(V->modelo, 50, stdin);
-    printf("\nENTRE COM O TIPO: ");
-    fflush(stdin);
-    fgets(V->tipo, 50, stdin);
-    printf("\nENTRE COM O COR: ");
-    fflush(stdin);
-    fgets(V->cor, 20, stdin);
-    printf("\nENTRE COM O TIPO DE COMBUSTIVEL: ");
-    fflush(stdin);
-    fgets(V->tipoCombustivel, 20, stdin);
-    printf("\nENTRE COM O ANO: ");
-    scanf("%d", &V->ano);
-    printf("\nENTRE COM A KILOMETRAGEM: ");
-    scanf("%f", &V->kilometragem);
-    printf("\nENTRE COM O VALOR DO ALUGUEL: ");
-    scanf("%f", &V->valorLocacao);
-    printf("\nENTRE COM A QUANTIDADE DE VEICULOS DESSE MODELO: ");
-    scanf("%d", &V->qtd);
-    printf("\n\n\n\nVEICULO CADASTRADO COM SUCESSO\n");
-    fflush(stdin);
-}
-
-
-void imprimirCliente(CLIENTE C){
-    printf("\nDADOS DO CLIENTE\n\n");
-    fflush(stdin);
-    printf("\nCODIGO DO CLIENTE: %d", C.codigo);
-    fflush(stdin);
-    printf("\nNOME DO CLIENTE: %s", C.nome);
-    fflush(stdin);
-    printf("\nDATA DE NASCIMENTO: %s", C.data_nasc);
-    fflush(stdin);
-    printf("\nCPF: %s", C.cpf);
-    fflush(stdin);
-    printf("\nENDERECO: %s", C.endereco);
-    fflush(stdin);
-    printf("\nTELEFONE: %s", C.telefone);
-    fflush(stdin);
-    printf("\nE-MAIL: %s", C.email);
-    fflush(stdin);
-    printf("\n--------------------------------\n");
-}
-
-
-void menu(){
-    printf("***** SEJA BEM-VINDO! *****\n\n");
-    printf("\tMENU\n\n");
-    printf("1 - CADASTRAR\n");
-    printf("2 - CONSULTAR\n");
-    printf("3 - LISTAR VEICULOS\n");
-    printf("4 - LISTAR CLIENTES\n");
-    printf("7 - SAIR\n");
-    printf("\n\nENTRE COM A OPCAO DESEJADA: ");
-}
-
-
-
-int main(){
-
-    CLIENTE X;
-    Lista C;
-    int i=2;
-    ponteiro p=NULL;
-    menu();
-
-
-    inicializarListaCliente(&C);
-    //testando fun��es
-
-   while(i--){
-        cadastrarCliente(&X);
-        inserirCliente(&C, X);
-    }
-
-    p = C.prim;
-
-    while(p!=C.ult){
-        imprimirCliente(p->prox->cli);
-        p=p->prox;
-    }
-    printf("\n FINAL DA LISTA\n ");
-
-    return 0;
+void menu() {
+  printf("***** SEJA BEM-VINDO! *****\n\n");
+  printf("\tMENU\n\n");
+  printf("1 - GERENCIAR CLIENTES\n");
+  printf("2 - GERENCIAR VEÍCULOS\n");
+  printf("3 - GERENCIAR LOCAÇÕES\n");
+  printf("4 - GERENCIAR MANUTENÇÃO\n");
+  printf("5 - PREVISÃO DE FATURAMENTO\n");
+  printf("6 - SAIR\n");
+  printf("\nENTRE COM A OPÇÃO DESEJADA: ");
 }
